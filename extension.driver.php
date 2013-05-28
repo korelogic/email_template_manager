ee3f66fe3e73053b9b5445fa97c0df468ca10be7
Michael Hay <michael.hay@korelogic.co.uk>
2013-05-28 11:32:08 +0100

    Add Post Data To Params

--- extension.driver.php
+++ extension.driver.php
@@ -113,6 +113,12 @@
 				$template->addParams(Array("etm-entry-id"=>$context['entry']->get('id')));
 				Symphony::Engine()->Page()->_param["etm-entry-id"] = $context['entry']->get('id');
 				$xml = $template->processDatasources();
+				
+				//Add POST as page parameters
+				foreach($context['fields'] as $field => $val){
+					$template->addParams(Array("etm-post-".$field => $val));
+					Symphony::Engine()->Page()->_param["etm-post-".$field] = $val;
+				}
 
 				$about = $context['event']->about();
 				General::array_to_xml($xml, Array("events"=>Array($about['name'] => Array("post-values" =>$context['fields']))));
